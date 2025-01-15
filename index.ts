632fa3e19617507d1a04b7eb5e793760339e3c14
Wix-Code <ogbonna428alex@gmail.com>
2025-01-15 01:15:50 -0800

    push

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express();
+
+app.get( "/", (req : any, res : any) => {
+    res.send({message: "api is running"})
+})
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+})
